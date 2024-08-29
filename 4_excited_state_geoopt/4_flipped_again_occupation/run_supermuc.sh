b7436b042a8057914d7e9883a0bc5e13db08162c
JWilhelm <jan.wilhelm@ur.de>
2024-08-29 23:12:33 +0200

    add geoopt ground state and excited state

---
+++ 4_excited_state_geoopt/4_flipped_again_occupation/run_supermuc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Job Name and Files (also --job-name)
+#SBATCH -J benzene
+
+#Output and error (also --output, --error):
+#SBATCH -o ./%x.%j.out
+#SBATCH -e ./%x.%j.err
+
+#Initial working directory (also --chdir):
+#SBATCH -D ./
+
+# Change here the job size and time limit:
+#SBATCH --time=00:30:00
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=48
+#SBATCH --partition=test
+#SBATCH --no-requeue
+#SBATCH --exclude=i01r01c01s[05,07-12],i01r01c02s[01-09],i01r01c02s[10-12],i01r01c03s[02-12],i01r01c04s[01-02],i01r01c03s01,i01r01c04s[03-12],i01r01c05s[01-05]
+
+#Setup of execution environment
+#SBATCH --export=NONE
+#SBATCH --get-user-env
+#SBATCH --account=pn36zo
+
+module load slurm_setup
+
+module load cp2k
+
+export OMP_NUM_THREADS=1
+
+mpiexec -n $SLURM_NTASKS cp2k.popt *inp &> cp2k.out
